72bfa84683ff2995b14c28ac187273a67d61ef96
Frey754 <139860198+Frey754@users.noreply.github.com>
2025-10-29 15:50:24 +0300

    feat: add smooth scrolling functionality

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    // плав прокрутка к секциям
+    const navLinks = document.querySelectorAll('.nav__menu a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - 80,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
